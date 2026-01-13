d9643c26007a735ff6fbc1841e283671dcf761fd
carlamunti <cmuntadag35@gmail.com>
2026-01-13 10:24:22 +0100

    MongoDB Atlas setup and Mongoose connection

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,19 +1,22 @@
-import express, { type Request, type Response } from "express";
+import express from "express";
 import dotenv from "dotenv";
 import cors from "cors";
+import { connectDB } from "./config/db.js";
 
 dotenv.config();
 
 const app = express();
 const PORT = process.env.PORT || 4000;
 
+connectDB();
+
 app.use(cors());
 app.use(express.json());
 
-app.get("/ping", (req: Request, res: Response) => {
-  res.json({ message: "pong", status: "OK" });
+app.get("/ping", (req, res) => {
+  res.json({ message: "pong" });
 });
 
 app.listen(PORT, () => {
-  console.log(`🚀 Servidor corriendo en http://localhost:${PORT}`);
+  console.log(`🚀 Servidor en http://localhost:${PORT}`);
 });
